7b7e0ea39d7c8984751ffd5a385a186cc4f2efa6
rsanders001 <rsanders2k@gmail.com>
2020-04-10 20:37:18 -0500

    Homework Commit

---
+++ TabbedPageWithNavigationPage/SettingsPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TabbedPageWithNavigationPage
+{
+	public partial class SettingsPage : ContentPage
+	{
+		public SettingsPage ()
+		{
+			InitializeComponent ();
+		}
+		private void OnClicked(Object sender, EventArgs e)
+		{
+			if(Convert.ToInt32(age.Text) < 16)
+			{
+				Output.Text = "Too Young To Drive";
+			}
+			else
+			{
+				var ageNumber = Convert.ToInt32(age.Text) - 16;
+				Output.Text = "You have been driving for " + ageNumber + "years";
+			}
+		}
+	}
+}
+
